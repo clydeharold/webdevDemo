3edc457047e56bb5a7b4a3cfaa698460378c8d1a
unknown <ch.paronda@gmail.com>
2019-10-22 17:08:22 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+      <vueSideBar/>
+      <div id="content">
+        <vueNavBar/>
+        <router-view/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import vueSideBar from './components/vueSideBar'
+import vueNavBar from './components/vueNavBar'
+import vuePageHome from './components/vuePageHome'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    vueSideBar,
+    vueNavBar,
+    vuePageHome
+  }
+}
+</script>
+
+<style src="./assets/css/style.css">
+</style>
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import Home from './components/vuePageHome.vue';
+import About from './components/vuePageAbout.vue';
+
+
+export const routes = [
+    {path: '/',component: Home},
+    {path: '/About',component:About},
+];
+
